a6ef178e55e6d788c89ceea24617fd174524bfc5
Victor Flávio P. Dornelos <victor.dornelos@hotmail.com>
2024-04-11 20:42:18 -0300

    complementando o final da análise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from calculadora_compra import Calculadora_compra
 from calculadora_assinatura import Calculo_assinatura
-from help import formata_float_str_moeda
+import time
 import pandas as pd
 def main():
     juros, custo_oportunidade, manutencao, depreciacao_real, valor_mercado, seguro, imposto, carro, metodo_pagamento, preco_carro, tempo = Calculadora_compra()
@@ -21,16 +21,61 @@ def main():
         'Assinatura': [valor_assinatura]
     }
     df = pd.DataFrame(data)
-    print(df['Preço do carro'])
-    saldo = (df['Preço do carro'] - df['Assinatura']) - (df['Preço do carro'] - df['Juros'] - df['Seguro'] + df['Depreciação real'] - df['Manutenção'] - df['Custo de oportunidade'] - df['Impostos'])
+    print(df['Preço do carro'].values[0])
+    saldo = (df['Preço do carro'].values[0] - df['Assinatura'].values[0]) - (df['Preço do carro'].values[0] - df['Juros'].values[0] - df['Seguro'].values[0] + df['Depreciação real'].values[0] - df['Manutenção'].values[0] - df['Custo de oportunidade'].values[0] - df['Impostos'].values[0])
     modulo_saldo = abs(saldo)
-    print(saldo)
 
     if saldo < 0:
-        print(f'A melhor opção é comprar o {df['Carro']}, com uma economia de  R$ {modulo_saldo}')
-    else:
-        print(f'A melhor opção é assinar o {df['Carro']}, com uma economia de R$ {saldo}')
+        print('Obrigado por todas informações!')
+        print('O resultado é')
+        time.sleep(1)
+        print('.')
+        time.sleep(1)
+        print('.')
+        time.sleep(1)
+        print('.')
+        print(f'A melhor opção é comprar o {df['Carro'].values[0]}, com uma economia de  R$ {modulo_saldo:,.2f}')
+        print('Esse gráfico pode ajudar em perceber quais são os maiores custos')
+        time.sleep(1)
+        print('.')
+        time.sleep(1)
+        print('.')
+        time.sleep(1)
+        print('.')
 
+        time.sleep(1)
+        print('.')
+        time.sleep(1)
+        print('.')
+        time.sleep(1)
+        print('.')
+        print('Obrigado!')
+        print('Volte sempre')
+    else:
+        print('Obrigado por todas informações!')
+        print('O resultado é')
+        time.sleep(1)
+        print('.')
+        time.sleep(1)
+        print('.')
+        time.sleep(1)
+        print('.')
+        print(f'A melhor opção é assinar o {df['Carro'].values[0]}, com uma economia de R$ {modulo_saldo:,.2f}')
+        time.sleep(1)
+        print('.')
+        time.sleep(1)
+        print('.')
+        time.sleep(1)
+        print('.')
+
+        time.sleep(1)
+        print('.')
+        time.sleep(1)
+        print('.')
+        time.sleep(1)
+        print('.')
+        print('Obrigado!')
+        print('Volte sempre')
 
 
 if __name__ =='__main__':
